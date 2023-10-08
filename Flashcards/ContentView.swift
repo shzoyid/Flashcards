d543e4e99d398afb9afd256683ad3fce198c2a53
shahad <shabduz10@gmail.com>
2023-10-08 16:54:04 +0300

    update

--- Flashcards/ContentView.swift
+++ Flashcards/ContentView.swift
@@ -9,15 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Image(.shahad)
+            .resizable()
+            .frame(width: 300, height: 300)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .padding(5)
+            .background(
+                Circle().strokeBorder(Color.yellow, lineWidth: 50)
+            )
+        VStack(alignment: .leading){
+            
+            Text("     Shahad Alzowaid")
+                .font(.largeTitle).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundStyle(.pink)
+            Text("Data Science & Analytics student and an IOS develepor trainee at ADA")
+
+
         }
-        .padding()
+        }
+        
     }
-}
+
 
 #Preview {
     ContentView()
