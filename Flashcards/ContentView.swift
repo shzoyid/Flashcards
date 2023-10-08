9c135e2c63781122f92ba269833c4196d31745a0
shahad <shabduz10@gmail.com>
2023-10-08 14:41:53 +0300

    Initial Commit

---
+++ Flashcards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flashcards
+//
+//  Created by Shahad Alzowaid on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Shahad Alzowaid on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
