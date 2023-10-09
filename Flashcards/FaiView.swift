a0f2783d52fc30b8bb7e0fd1a2921abc991bed62
fai <falotaibi23@twq.idserve.net>
2023-10-09 15:12:09 +0300

    My updates

---
+++ Flashcards/FaiView.swift
@@ -0,0 +1,18 @@
+//
+//  FaiView.swift
+//  Flashcards
+//
+//  Created by Fai Talaq Aladhyani on 09/10/2023.
+//
+
+import SwiftUI
+
+struct FaiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaiView()
+}
